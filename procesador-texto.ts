8d95cc60078e6684f83e8bb5206f9dc152cec285
amriberfrontend <239142012+amriberfrontend@users.noreply.github.com>
2025-12-18 09:34:55 +0000

    Añadido archivo procesador-texto.ts

---
+++ procesador-texto.ts
@@ -0,0 +1,16 @@
+const PALABROS: string[] = [
+    "ababol",
+    "cenutrio",
+    "cabron"
+]
+
+export function extraerPalabros(texto:string): string[] {
+    // sacar palabros del texto    
+    // eliminar puntuación
+    // separar texto en array
+    // crear nuevo array
+}
+
+export function calcularPesoPalabros(palabros: string[]): number {
+    // comprobar el peso de cada palabro y devolver la suma
+}
