a3681761c07bb8eb19d5e77e6ed3a8d85d51a5be
andresaragonguerrero <andresaragonguerrero@gmail.com>
2025-12-18 10:30:29 +0000

    se implementó la función calcularPesoPalabros

--- procesador-texto.ts
+++ procesador-texto.ts
@@ -1,10 +1,10 @@
-const PALABROS: {palabro: string, peso: number}[] = [
-    {palabro: "ababol", peso: 1},
-    {palabro: "cenutrio", peso: 5},
-    {palabro: "cabron", peso: 10},
+const PALABROS: { palabro: string, peso: number }[] = [
+    { palabro: "ababol", peso: 1 },
+    { palabro: "cenutrio", peso: 5 },
+    { palabro: "cabron", peso: 10 },
 ]
 
-export function extraerPalabros(texto:string): string[] {
+export function extraerPalabros(texto: string): string[] {
     // sacar palabros del texto    
     // eliminar puntuación
     // separar texto en array
@@ -13,4 +13,12 @@ export function extraerPalabros(texto:string): string[] {
 
 export function calcularPesoPalabros(palabros: string[]): number {
     // comprobar el peso de cada palabro y devolver la suma
+    let suma = 0;
+    for (const palabro of palabros) {
+        const palabroEncontrado = PALABROS.find(p => p.palabro === palabro);
+        if (palabroEncontrado) {
+            suma += palabroEncontrado.peso;
+        }
+    }
+    return suma;
 }
